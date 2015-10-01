06791a6f5b0988c760c79e06526654490d2075a2
B. Arman Aksoy <arman@aksoy.org>
2015-10-01 14:22:56 -0400

    seed the test folder with some dummy mhctools code

---
+++ test/python/test-mhctools.py
@@ -0,0 +1,23 @@
+# Downloaded from https://github.com/hammerlab/mhctools#Example
+
+from mhctools import NetMHCpan
+# Run NetMHCpan for alleles HLA-A*01:01 and HLA-A*02:01
+predictor = NetMHCpan(alleles=["A*02:01", "hla-a0101"])
+
+# scan the short proteins 1L2Y and 1L3Y for epitopes
+protein_sequences = {
+		  "1L2Y": "NLYIQWLKDGGPSSGRPPPS",
+		    "1L3Y": "ECDTINCERYNGQVCGGPGRGLCFCGKCRCHPGFEGSACQA"
+		    }
+
+epitope_collection = predictor.predict(protein_sequences)
+
+# flatten binding predictions into a Pandas DataFrame
+df = epitope_collection.dataframe()
+
+# epitope collection is sorted by percentile rank
+# of binding predictions
+strongest_predicted_binder = epitope_collection[0]
+
+# should be NLYIQWLKDGGPSSGRPPPS 
+print strongest_predicted_binder.source_sequence
